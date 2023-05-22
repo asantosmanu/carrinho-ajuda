7d0f9ce45893391e3c34f2453649c9380f957ed7
asantosmanu <manuelasantos452006@gmail.com>
2023-05-22 11:59:49 -0300

    terminar-css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-
+import { livros } from '@/_data/livros'
+//exportando do livro.js E importanto para o app
 </script>
 
 <template>
-  
+  <h1>Minha Livraria</h1>
+  <div class="livros">
+    <div class="card-livro" v-for="livro in livros" :key="livro.id">
+        <img :src="livro.cover" class="livro-cover" >
+      <p class="livro-title">{{ livro.title }}</p>
+      <p class="livro-author">{{ livro.author }}</p>
+      <p class="livro-price">{{ livro.price }}</p>  
+    </div>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/_data/livros.js
@@ -0,0 +1,39 @@
+export const livros = [
+    {
+        id: 1,
+        title: "O poderoso chefão",
+        author: "Mario Puzo",
+        price: 49.90,
+        cover: "https://m.media-amazon.com/images/I/51dCp7dq93L.jpg"
+    },
+        //em cover: "copiar endereço da imagem"
+    {
+        id: 2,
+        title: "O senhor dos anéis",
+        author: "J. R. R. Tolkien",
+        price: 29.90,
+        cover: "https://m.media-amazon.com/images/I/71ZLavBjpRL._AC_UY218_.jpg"
+    },
+    {
+        id: 3,
+        title: "Bridgerton",
+        author: "Julia Quinn",
+        price: 87.90,
+        cover: "https://m.media-amazon.com/images/I/51dCp7dq93L.jpg"
+    },
+    {
+        id: 4,
+        title: "Os sete maridos de Evelyn Hugo (Portuguese Edition)",
+        author: "Taylor Jenkins Reid",
+        price: 59.90,
+        cover: "https://m.media-amazon.com/images/I/91aRbGB8IFL._AC_UY218_.jpg"
+    },
+    {
+        id: 5,
+        title: "Alice no Pais das Maravilhas ",
+        author: "Lewis Carroll",
+        price: 59.90,
+        cover: "https://m.media-amazon.com/images/I/61AD--JL1KL._SX331_BO1,204,203,200_.jpg"
+    },
+    
+]
